7523bce19bae85e7d0ec73a4ae78bf246dbd9a48
juanib99 <juanignaciobisello@hotmail.com>
2025-08-11 11:01:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { AuthLayout } from './auth/layout/AuthLayout'
+import ChatLayout from './chat/layout/ChatLayout'
+import ChatPage from './chat/layout/pages/ChatPage'
+
+function App() {
+
+  return (
+    <>
+      {/* <AuthLayout /> */}
+      <ChatLayout >
+        <ChatPage />
+      </ChatLayout>
+    </>
+  )
+}
+
+export default App
