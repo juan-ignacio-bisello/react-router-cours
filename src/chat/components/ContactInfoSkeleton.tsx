f2fcdfce91d382295d03721d0e463789694dfe7a
juanib99 <juanignaciobisello@hotmail.com>
2025-08-14 00:50:20 -0300

    separacion de componmentes adicionales

---
+++ src/chat/components/ContactInfoSkeleton.tsx
@@ -0,0 +1,7 @@
+
+
+export const ContactInfoSkeleton = () => {
+  return (
+    <div>ContactInfoSkeleton</div>
+  )
+}
---
+++ src/chat/components/NoContactSelected.tsx
@@ -0,0 +1,9 @@
+
+
+export const NoContactSelected = () => {
+  return (
+    <div
+        className="flex-1 flex items-center justify-center text-muted-foreground"
+    >Seleccione un cliente</div>
+  )
+}
